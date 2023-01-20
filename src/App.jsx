23e5bbd38cd83ddf33c3326e874e82b4b226ac87
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-19 18:34:49 -0600

    useMemo to cache expenisve function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import UseRefExample from './components/UseRefExample'
 import UseRefExample2 from './components/UseRefExample2'
 import UseRefMemLeak from './components/UseRefMemLeak'
+import UseMemoExample from './components/UseMemoExample'
 
 function App() {
 
 
   return (
     <div className="App">
-    <UseRefMemLeak/>
+    <UseMemoExample/>
     </div>
   )
 }
